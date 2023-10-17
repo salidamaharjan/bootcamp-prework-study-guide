0d20998810858100cfb63308f38513bd5df66f96
salidamaharjan <salidamaharjan@outlook.com>
2023-10-16 22:32:57 -0500

    using if statement

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,14 @@
-var topics = "HTML, CSS, Git, JavaScript";
-console.log(topics);
+var topic = "HTML";
+
+if (topic === "HTML") {
+    console.log("Let's study HTML!");
+} else if (topic === "CSS") {
+    console.log("Let's study CSS!");
+}
+ else if (topic === "Git") {
+    console.log("Let's study Git!");
+ } else if (topic === "JavaScript") {
+    console.log("Let's study JAvaScript!");
+ } else {
+    console.log("Plaese try again!");
+ }
